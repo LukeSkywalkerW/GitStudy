0ec617ce18e816387f4d98f9fa80a2a4a6eb2f9c
Velaryon-w <929195749@qq.com>
2020-10-07 10:44:18 +0800

    411

---
+++ src/MainClass.java
@@ -0,0 +1,8 @@
+
+public class MainClass {
+	public static void main(String[] args) {
+		int i = 21;
+		System.out.println("i << 2 :" +(i << 2));//左移两位，乘以四
+		System.out.println("i << 3 :" +(i << 3));
+	}
+}
